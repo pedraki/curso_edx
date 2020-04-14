abb72c78f70b77d5926bf6c8ed20e33c93441920
unknown <pedro.gmerino@gmail.com>
2020-04-14 17:52:20 +0200

    ttt1 ganador

--- edx/C1HW2/ttt1.py
+++ edx/C1HW2/ttt1.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 
+
 def create_board():
 	return(np.zeros((3,3)))
 
@@ -16,21 +17,47 @@ def random_place(board,player):
 	librex,librey=possibilities(board)
 	rx=np.random.choice(librex)
 	ry=np.random.choice(librey)
-	return(rx,ry)
-
-
-
-X=create_board()
-player=1
-for x in range(9):
-	posicion=random_place(X,player)
-	if player==1:
-		player=2
+	if board[rx][ry]==0:
+		board[rx][ry]=player
+	
+def row_win(board,player):
+	if np.any(np.all(board==player,axis=0)==True):
+		return True
 	else:
-		player=1
+		return False
 
-	print(posicion)
-	place(X,player,posicion)
+def col_win(board,player):
+	if np.any(np.all(board==player,axis=1)==True):
+		return True
+	else:
+		return False
+
+def diag_win(board,player):
+	if np.all(np.diag(board,0)==player) or np.all(np.diag(np.fliplr(board),0)==player):
+		return True
+	else:
+		return False
+
+
+
+np.random.seed(1)
+X=create_board()
+X[1][1]=1
+
+ganador=False
+
+while not ganador:
+	for player in [1,2]:
+		posicion=random_place(X,player)
+		if player==1:
+			player=2
+		else:
+			player=1
+		if  row_win(X,player) or col_win(X,player) or diag_win(X,player):
+			print ("el ganador es player ", player)	
+			ganador=True
+			break
+	
 
 print(X)
 # place(X,1,posicion)
