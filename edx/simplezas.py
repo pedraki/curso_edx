c8fa1bbbfe86a02c55b2d95761f42fcf224d7ba8
admon <pedro.gmerino@gmail.com>
2020-04-25 12:44:00 +0200

    xml 02

--- edx/simplezas.py
+++ edx/simplezas.py
@@ -1,3 +1,6 @@
-a='pedro'
-b='Hola %s que tal estas' %a
-print(b)
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth() # client_secrets.json need to be in the same directory as the script
+drive = GoogleDrive(gauth)
