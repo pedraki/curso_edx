5df30344a4ff3fb83f37f8e657c0cb9e2d106573
unknown <pedro.gmerino@gmail.com>
2020-04-26 13:18:38 +0200

    vida m

--- edx/GAMES/vida01.py
+++ edx/GAMES/vida01.py
@@ -30,17 +30,16 @@ gameState[20,23]=1
 pauseExect=False
 juego=True
 while juego:
-
 	newgameState=np.copy(gameState)
-	screen.fill(bg)
-	time.sleep(0.1)
-	ev=pygame.event.get()
-	for evento in ev:
+
+	for evento in pygame.event.get():
 		if evento.type == pygame.QUIT:
 			juego=False
+
 		#pygame.KEYDOWM ES AL bajar CUALQUIER TECLA keyup es al subir
 		if evento.type == pygame.KEYDOWN:
 			pauseExect = not pauseExect
+
 	#mouseClick tupla (izdo,central, derecho) 0 no pulsado 1 pulsado
 		mouseClick=pygame.mouse.get_pressed()
 		if sum(mouseClick) > 0:
@@ -51,13 +50,12 @@ while juego:
 			newgameState[celX,celY]=not mouseClick[2]
 		
 
-	
+	screen.fill(bg)
 		
 	for y in range(nxC):
 		for x in range(nyC):
 			if not pauseExect:
-			
-			#numero de vecinos
+
 				n_neigh = gameState[(x-1) % nxC, (y+1) % nyC]+ \
 						  gameState[(x)   % nxC, (y+1) % nyC]+ \
 						  gameState[(x+1) % nxC, (y+1) % nyC]+ \
@@ -67,26 +65,26 @@ while juego:
 						  gameState[(x)   % nxC, (y-1) % nyC]+ \
 						  gameState[(x+1) % nxC, (y-1) % nyC] 
 
-		#muerta con 3 vecinas vivas revive
+
 				if gameState[x,y]==0 and n_neigh==3:
 					newgameState[x,y]=1
-	#viva con menos de 2 o mas de 3 vecinas vivas muere
 				elif gameState[x,y] == 1 and (n_neigh < 2 or n_neigh > 3):
 					newgameState[x,y] =0
 
-				poly=[
-				((x) * dimcW, y * dimcH), 
-				((x+1) * dimcW , y * dimcH),
-				((x+1) * dimcW ,(y+1) * dimcH),
-				((x) * dimcW ,(y+1) * dimcH)
-				]
-					
-				#pantalla, color, punto,ancho
-				if newgameState[x,y]==0:
-					pygame.draw.polygon(screen,(128,128,128),poly,1)
-				else:
-					pygame.draw.polygon(screen,(255,255,255),poly,0)
+			poly=[
+			((x) * dimcW, y * dimcH), 
+			((x+1) * dimcW , y * dimcH),
+			((x+1) * dimcW ,(y+1) * dimcH),
+			((x) * dimcW ,(y+1) * dimcH)
+			]
+				
+			#pantalla, color, punto,ancho
+			if newgameState[x,y]==0:
+				pygame.draw.polygon(screen,(128,128,128),poly,1)
+			else:
+				pygame.draw.polygon(screen,(255,255,255),poly,0)
 
-		gameState=np.copy(newgameState)
-		pygame.display.flip()
+	gameState=np.copy(newgameState)
+	time.sleep(0.1)
+	pygame.display.flip()
 	
